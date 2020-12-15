104bf8aad9d57ba914ae1f11e4a8c930c3e3d050
Faithfulness Alamu <vaguemail369@gmail.com>
2020-12-15 22:31:07 +0100

    Improve server instantiation

--- server.go
+++ server.go
@@ -7,39 +7,17 @@ import (
 	"time"
 )
 
-type server struct {
-	searcher *Searcher
-	srv      *http.Server
+type Server struct {
+	*http.Server
 }
 
-func newServer(s *Searcher) *server {
-	return &server{
-		s,
-		&http.Server{},
-	}
+func (s *Server) Start() error {
+	fmt.Printf("Listening on %s...", s.Addr)
+	return s.ListenAndServe()
 }
 
-func (s *server) start(addr string) error {
-	fmt.Printf("Listening on %s...", addr)
-	s.srv.Handler = s.routes()
-	s.srv.Addr = addr
-
-	return s.srv.ListenAndServe()
-}
-
-func (s *server) stop() error {
+func (s *Server) Stop() error {
 	shutdownCtx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
-
-	return s.srv.Shutdown(shutdownCtx)
-}
-
-func (s *server) routes() http.Handler {
-	mux := http.NewServeMux()
-
-	fs := http.FileServer(http.Dir("./static"))
-	mux.Handle("/", fs)
-	mux.HandleFunc("/search", handleSearch(s.searcher))
-
-	return mux
+	return s.Shutdown(shutdownCtx)
 }
